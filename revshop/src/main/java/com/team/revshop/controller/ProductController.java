d171a84e5de21ae65b20819285b63fe5765cc297
Kaprister <sushantkapri@gamil.com>
2024-10-16 11:42:38 +0530

    initial commit

---
+++ revshop/src/main/java/com/team/revshop/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.team.revshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/")
+    public String greet(){
+        return "Hello World";
+    }
+}
